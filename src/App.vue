b3b4602a9e6a2a94c99c13ea8479027d55282861
Shen0000 <73674912+Shen0000@users.noreply.github.com>
2022-03-03 22:58:38 -0500

    add basic functionality and fix v-select component rendering

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,25 @@
   <div class="main-style">
   <p>hello</p>
   {{currencies}}
-  <div class="vselect">
-      <v-select class="dropdown" placeholder="Base Currency:" v-model="option" :options="currencies"  />
-      <!-- <form v-on:submit.prevent="submitForm">
-        <br>
-        <h2>Handle:</h2>
-        <br>
-        <div class="form-group">
-            <input type="text" class="form-control" id="handle" placeholder="" v-model="form.handle">
-        </div>
-        <input type="submit" value="submit">
-      </form> -->
+  <!-- {{data}} -->
+  <div class="converter-wrapper">
+    <div class="vselect">
+        <v-select class="dropdown" placeholder="Base Currency:" v-model="base" :options="currencies"  />
+        <v-select class="dropdown" placeholder="Target Currency:" v-model="target" :options="currencies"  />
+        <form v-on:submit.prevent="submitForm">
+          <!-- <br>
+          <h2>Handle:</h2>
+          <br>
+          <div class="form-group">
+              <input type="text" class="form-control" id="handle" placeholder="" v-model="form.handle">
+          </div> -->
+          <input type="submit" value="Convert!">
+        </form>
+      </div>
     </div>
+    <!-- {{target}} -->
+    <h1 v-if="display_target != display_base">1 {{display_base}} = {{data['data'][display_target]}} {{display_target}}</h1>
+    <h1 v-else>convert to get results</h1>
   </div>
 </template>
 
@@ -27,9 +34,12 @@ export default {
   data() {
     return {
       data: {},
-      currencies: {},
-      base : "BTC",
-      option: "USD",
+      currencies: [ "USD", "JPY", "CNY", "CHF", "CAD", "MXN", "INR", "BRL", "RUB", "KRW", "IDR", "TRY", "SAR", "SEK", "NGN", "PLN", "ARS", "NOK", "TWD", "IRR", "AED", "COP", "THB", "ZAR", "DKK", "MYR", "SGD", "ILS", "HKD", "EGP", "PHP", "CLP", "PKR", "IQD", "DZD", "KZT", "QAR", "CZK", "PEN", "RON", "VND", "BDT", "HUF", "UAH", "AOA", "MAD", "OMR", "CUC", "BYR", "AZN", "LKR", "SDG", "SYP", "MMK", "DOP", "UZS", "KES", "GTQ", "URY", "HRV", "MOP", "ETB", "CRC", "TZS", "TMT", "TND", "PAB", "LBP", "RSD", "LYD", "GHS", "YER", "BOB", "BHD", "CDF", "PYG", "UGX", "SVC", "TTD", "AFN", "NPR", "HNL", "BIH", "BND", "ISK", "KHR", "GEL", "MZN", "BWP", "PGK", "JMD", "XAF", "NAD", "ALL", "SSP", "MUR", "MNT", "NIO", "LAK", "MKD", "AMD", "MGA", "XPF", "TJS", "HTG", "BSD", "MDL", "RWF", "KGS", "GNF", "SRD", "SLL", "XOF", "MWK", "FJD", "ERN", "SZL", "GYD", "BIF", "KYD", "MVR", "LSL", "LRD", "CVE", "DJF", "SCR", "SOS", "GMD", "KMF", "STD", "BTC", "XRP", "AUD", "BGN", "JOD", "GBP", "ETH", "EUR", "LTC", "NZD" ],
+      prev_base: "",
+      base : "",
+      target: "",
+      display_base: "",
+      display_target: "",
     };
   },
   methods: {
@@ -38,16 +48,35 @@ export default {
       .then((response) => response.json())
       .then((data) => {
         this.data = data;
-        this.currencies = Object.keys(this.data.data);
+        console.log("Fetching data...")
+        // this.currencies = Object.keys(this.data.data);
       })
       .catch(error => {
         console.error(error);
       })
-    }
+    },
+    submitForm() {
+      if (!this.base && !this.target) {
+        alert("Please enter a base and a target");
+      }
+      else if (!this.base) {
+        alert("Please enter a base");
+      }
+      else if (!this.target) {
+        alert("Please enter a target");
+      }
+      else {
+        if (this.base != this.prev_base) {
+          this.fetchData();
+        }
+        this.prev_base = this.base; // keep track of previous base currency to minimize API calls
+        this.display_base = this.base;
+        this.display_target = this.target;
+      }
+    },
   },
   created() {
     this.fetchData();
-  
   }
 }
 </script>
@@ -63,7 +92,7 @@ export default {
   --vs-dropdown-option-color: #cc99cd;
 
   --vs-selected-bg: #664cc3;
-  --vs-selected-color: #eeeeee;
+  --vs-selected-color: #201d1d;
 
   --vs-search-input-color: #eeeeee;
 
@@ -79,4 +108,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.dropdown {
+  /* max-width: 50%; */
+  position: relative;
+  display: inline-block;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import App from './App.vue'
 import vSelect from 'vue-select'
 import 'vue-select/dist/vue-select.css'
 var app = createApp(App);
-app.mount("#app");
 app.component("v-select", vSelect);
+app.mount("#app");
